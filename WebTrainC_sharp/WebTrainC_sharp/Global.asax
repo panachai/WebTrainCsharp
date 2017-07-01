6eaf1a7af7012422f4ebf263ddf3af7dc45d587a
Panachai <panachai.ny@gmail.com>
2017-07-01 18:18:29 +0700

    origin

---
+++ WebTrainC_sharp/WebTrainC_sharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTrainC_sharp.MvcApplication" Language="C#" %>
---
+++ WebTrainC_sharp/WebTrainC_sharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTrainC_sharp.Startup))]
+namespace WebTrainC_sharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
